d93887c747298aaf222111a8367ebcf445fae68c
Danila Emelyanov <62074408+jezmunh@users.noreply.github.com>
2023-02-08 17:10:19 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -10,14 +10,10 @@ if(localStorage.getItem("city") == '') {
 city = localStorage.getItem("city");
 let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&lang=${lang}&units=metric&appid=${apiKey}`;
 axios.get(url).then(res => {
-    console.log(res.data)
-        // Вывод города
-    document.querySelector('.city').innerHTML = res.data.name
-        // Вывод температуры
-    document.querySelector('.temp').innerHTML = res.data.main.temp
-        // Вывод влажности
-    document.querySelector('.humidity').innerHTML = res.data.main.humidity
-        // Вывод скорости ветра
+    console.log(res.data)        
+    document.querySelector('.city').innerHTML = res.data.name        
+    document.querySelector('.temp').innerHTML = res.data.main.temp        
+    document.querySelector('.humidity').innerHTML = res.data.main.humidity        
     document.querySelector('.wind').innerHTML = res.data.wind.speed
     document.querySelector('.description').innerHTML = res.data.weather[0].description
     
@@ -30,22 +26,16 @@ change_link.onclick = function () {
     let input_city = prompt('Enter city:')
     city = input_city
     localStorage.setItem("city", city)
-
-    city = localStorage.getItem("city")
-    
+    city = localStorage.getItem("city")    
     axios.get(url).then(res => {
-    console.log(res.data)
-        // Вывод города
-    document.querySelector('.city').innerHTML = res.data.name
-        // Вывод температуры
-    document.querySelector('.temp').innerHTML = res.data.main.temp
-        // Вывод влажности
-    document.querySelector('.humidity').innerHTML = res.data.main.humidity
-        // Вывод скорости ветра
+    console.log(res.data)       
+    document.querySelector('.city').innerHTML = res.data.name        
+    document.querySelector('.temp').innerHTML = res.data.main.temp       
+    document.querySelector('.humidity').innerHTML = res.data.main.humidity        
     document.querySelector('.wind').innerHTML = res.data.wind.speed
-    document.querySelector('.description').innerHTML = res.data.weather[0].description
-    
+    document.querySelector('.description').innerHTML = res.data.weather[0].description    
     location.href = location
+        
      })
 }
 
